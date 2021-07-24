2398dbac7bb05486f624663853074111af88fb1d
PWinwon <p9a3r5k0@gmail.com>
2021-07-24 10:38:49 +0900

    0724 update swea_1974 sdoqu

--- swea/d2/swea_1974.py
+++ swea/d2/swea_1974.py
@@ -1,52 +1,39 @@
-def line_check(lines):
-    check = []
-    for j in range(9):
-        if check:
-            if lines[j] in check:
-                return 0
-        check.append(list[j])
-    return 1
-
-
-def sdoqu_mir(s):
-    result = s
-    for x in range(9):
-        for y in range(9):
-            result[x][y] = s[y][x]
-    return result
-
-
 T = int(input())
 
 for i in range(T):
     sdoqu = []
     for j in range(9):
-        sample_list = list(map(int, input().split()))
-        sdoqu.append(sample_list)
+        sample_ls = list(map(int, input().split()))
+        sdoqu.append(sample_ls)
+    # print(sdoqu)
     result = 1
-    m_sdoqu = sdoqu_mir(sdoqu)
-    for sdo in sdoqu:
-        if line_check(sdo):
-            continue
-        else:
-            result = 0
-            break
-    for m in m_sdoqu:
-        if line_check(sdo):
-            continue
-        else:
-            result = 0
-            break
+    for x in range(9):
+        check_ls = []
+        for y in range(9):
+            if check_ls:
+                if sdoqu[x][y] in check_ls:
+                    result = 0
+                    break
+            check_ls.append(sdoqu[x][y])
+    
+    for x in range(9):
+        check_ls = []
+        for y in range(9):
+            if check_ls:
+                if sdoqu[y][x] in check_ls:
+                    result = 0
+                    break
+            check_ls.append(sdoqu[y][x])
+    
     for x in range(0,9,3):
         for y in range(0,9,3):
-            sqare_list = []
+            check_ls = []
             for ck_x in range(x,x+3):
-                for ck_y in range(y, y+3):
-                    sqare_list.append(sdoqu[ck_x][ck_y])
-            if line_check(sdo):
-                continue
-            else:
-                result = 0
-                break
+                for ck_y in range(y,y+3):
+                    if check_ls:
+                        if sdoqu[ck_x][ck_y] in check_ls:
+                            result = 0
+                            break
+                    check_ls.append(sdoqu[ck_x][ck_y])
     print(f'#{i+1} {result}')
 
