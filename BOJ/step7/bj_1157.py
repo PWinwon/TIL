a65e80d351c8be9e528f6a2288e6af13d0fdd14f
PWinwon <p9a3r5k0@gmail.com>
2021-08-07 23:43:47 +0900

    add 0807 boj

--- BOJ/step7/bj_1157.py
+++ BOJ/step7/bj_1157.py
@@ -9,3 +9,18 @@ for char in chars:
     else:
         result[char] = 1
 
+max_val = 0
+for val in result.values():
+    if val > max_val:
+        max_val = val
+
+
+max_list = []
+for k, val in result.items():
+    if val == max_val:
+        max_list.append(k)
+
+if len(max_list) == 1:
+    print(max_list[0])
+else:
+    print('?')
