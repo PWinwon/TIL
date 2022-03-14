22e44b1f2c27b11e9013337b6c1db86e8285f79a
PWinwon <p9a3r5k0@gmail.com>
2022-03-14 23:21:53 +0900

    add 0314 boj

--- Problem-Solving/BOJ/class3/BOJ_11726.py
+++ Problem-Solving/BOJ/class3/BOJ_11726.py
@@ -3,11 +3,10 @@ dp = [0] * (N+1)
 dp[1] = 1
 dp[2] = 2
 
-if N < 3:
+if N <= 3:
     print(dp[N])
 else:
     for n in range(3, N+1):
         dp[n] = dp[n-1] + dp[n-2]
-    
-    
-    print(dp[N])
+
+    print(dp[N]%10007)
