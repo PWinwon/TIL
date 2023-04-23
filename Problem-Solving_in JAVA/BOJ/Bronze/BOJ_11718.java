bcb9ff39ea91126e21517aca93a6d6602a8cb894
PWinwon <p9a3r5k0@gmail.com>
2023-04-23 20:45:59 +0900

    add 0423 boj java

---
+++ Problem-Solving_in JAVA/BOJ/Bronze/BOJ_11718.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_11718 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		
+//		while (sc.hasNext()) {
+//			System.out.println(sc.)
+//		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = "";
+		
+		while ((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+
+	}
+
+}
--- Problem-Solving_in JAVA/BOJ/Bronze/README.md
+++ Problem-Solving_in JAVA/BOJ/Bronze/README.md
@@ -1,5 +1,6 @@
 # 문제링크
 
+- [BOJ_11718(그대로 출력하기)](https://www.acmicpc.net/problem/11718)
 - [BOJ_1546(평균)](https://www.acmicpc.net/problem/1546)
 - [BOJ_2743(단어 길이 재기)](https://www.acmicpc.net/problem/2743)
 - [BOJ_2750(수 정렬하기)](https://www.acmicpc.net/problem/2750)
