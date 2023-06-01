f464f19170861e854fc6935fd100ea520e14771e
PWinwon <p9a3r5k0@gmail.com>
2023-06-01 21:28:22 +0900

    add 0601 boj java

---
+++ Problem-Solving_in JAVA/BOJ/Bronze/BOJ_7287.java
@@ -0,0 +1,11 @@
+
+public class BOJ_7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("390");
+		System.out.println("p9a3r5k0");
+
+	}
+
+}
